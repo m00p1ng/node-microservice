40ceb9eee2dc5b21c63be8e432a5b5d716270a9e
m00p1ng <mongkonchai4412@gmail.com>
2020-08-21 18:28:55 +0700

    add nats from tickets service

--- ticketing/tickets/src/routes/update.ts
+++ ticketing/tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotFoundError,
 } from '@mooping/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -35,8 +37,13 @@ router.put(
       title: req.body.title,
       price: req.body.price,
     });
-
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   },
