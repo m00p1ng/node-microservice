46bedd149042b28716b6536ee466b4f71f4da8cb
m00p1ng <mongkonchai4412@gmail.com>
2020-08-21 18:57:41 +0700

    add order service

---
+++ ticketing/orders/src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@mooping/common';
+
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { createOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+app.use(currentUser);
+
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(createOrderRouter);
+app.use(showOrderRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
