ff66b9241af993fcf6b8db89f0ac4a1b4e2c92fe
m00p1ng <mongkonchai4412@gmail.com>
2020-08-22 20:18:05 +0700

    add listener on orders

--- ticketing/tickets/src/routes/__tests__/update.test.ts
+++ ticketing/tickets/src/routes/__tests__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
@@ -110,6 +111,32 @@ it('published an event', async () => {
       price: 100,
     })
     .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('rejects updates if the ticket is reserved', async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'asldf',
+      price: 20,
+    });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100,
+    })
+    .expect(400);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
